649d5213d93742dd5d527e7a52390a17345a14d4
cammls <camille.malis@coding-academy.fr>
2017-04-22 20:03:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>TIC TAC TOE GAME</h2>
+        </div>
+        <p className="App-intro">
+          blablabla
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
